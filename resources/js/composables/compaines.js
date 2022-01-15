c7b961c509756fa3fd9380cc4357586c25ad23d7
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2022-01-15 19:15:16 +0200

    back-apis

---
+++ resources/js/composables/compaines.js
@@ -0,0 +1,24 @@
+import { ref } from 'vue';
+
+import axios from "axios";
+
+export default function useCompanies() {
+    const companies = ref([])
+
+
+    const getCompanies = async () => {
+        let response = await axios.get('/api/companies')
+        companies.value = response.data.data;
+    }
+
+    const destroyCompany = async (id) => {
+          await axios.delete('/api/companies/' + id)
+    }
+
+    return {
+        companies,
+        getCompanies,
+        destroyCompany
+    }
+
+}
